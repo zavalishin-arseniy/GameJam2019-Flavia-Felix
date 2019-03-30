eaf185b5e736cbcfd382c9d8be0f5401967fe4ac
KrylovBoris <hakkanx@gmail.com>
2019-03-30 23:48:56 +0300

    Added camera follow

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 lastPosition;
+    private Vector3 delta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null) Debug.LogError("Target for camera isn't set!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta = target.position - lastPosition;
+    }
+
+    void LateUpdate()
+    {
+        transform.position += delta;
+        lastPosition = target.position;
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class InputManager : MonoBehaviour
 {
     Vector3 inputMovement;
     private Movement movSystem;
+    private Aim aimSystem;
 
     void Awake()
     {
         movSystem = GetComponent<Movement>();
+        aimSystem = GetComponent<Aim>();
     }
 
     // Update is called once per frame
@@ -17,6 +20,7 @@ public class InputManager : MonoBehaviour
     {
         inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         movSystem.MovementVector = inputMovement;
+        aimSystem.FaceForwardDirection(Input.mousePosition);
     }
 }
 
