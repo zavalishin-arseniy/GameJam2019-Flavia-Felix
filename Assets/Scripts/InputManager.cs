1322585c0dec5eacb71360bc18b2d2c7cd1c3be2
KrylovBoris <hakkanx@gmail.com>
2019-03-30 21:34:21 +0300

    Movement added

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    Vector3 inputMovement;
+    private Movement movSystem;
+
+    void Awake()
+    {
+        movSystem = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movSystem.MovementVector = inputMovement;
+    }
+}
+
